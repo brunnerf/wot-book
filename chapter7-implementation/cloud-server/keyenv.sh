987fdd193424ffe892cacb4c987f394026a0a67c
brunnerf <brunner_felix@bluewin.ch>
2018-03-18 18:19:44 +0100

    version with keys

---
+++ chapter7-implementation/cloud-server/keyenv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export SERVER="https://api.evrythng.com"
+export EVRYTHNG_API_KEY="wa3IEcyzcn2ibVjvXxwuAG3eZhMWWx2ndjSTMNy8KyeDNxQPlcpIRstz45OsbgwyGA4qloLVP6CAPY2L"
+export PROJECT_ID="UH4WFEaHBgPaQKawahQA4bKg"
+export PRODUCT_ID="UnnWFHGWeg8wQpaRwXKaMmKc"
+export APP_ID="UH4fFGEqVD8RQKRaRkhyTp2g"
+export APP_API_KEY="cvJ5rXGRG3NGnw6VyAWBbmzXyM5bVauRJoD7gdoqPYagP06WYVim8nFYxHM4wSRHomRHIlr6OHi6WV6l"
+export THNG_ID="U4HCFp9pBDPatKRaRDchweXn"
+export THNG_API_KEY="UicrvSAumMNPrU3TdzoFLvRJLZqVWa3y7mCISPjGLm8heZAwFDiX10CdAE9Ln1s0E2jMKNDPBdTrcxBH"
